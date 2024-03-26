5fb97e8194b5b19f3e36be50f490d1333394c3b6
shikaan <spagnolo.manu@gmail.com>
2024-03-26 08:58:57 +0100

    docs: add inline documentation for the template and update engine to latest version

--- src/characters/Fish.cpp
+++ src/characters/Fish.cpp
@@ -1,4 +1,3 @@
-#include <cstdio>
 #include <latebit/EventOut.h>
 #include <latebit/EventStep.h>
 #include <latebit/Object.h>
@@ -9,15 +8,39 @@ using namespace lb;
 class Bubbles : public Object {
 public:
   Bubbles(Vector position) {
+    // This tiny object is a bubble that floats up the screen and disappears
+    // when it reaches the top. We give it a type and assign it a sprite that we
+    // have loaded in the main.cpp. Again you can pick the sprites by their
+    // label ("bubbles" in this case)
     setType("Bubbles");
     setSprite("bubbles");
+
+    // latebit supports collisions between objects. Marking this object as
+    // SPECTRAL means that it will not collide with other objects. You can read
+    // more about solidness in latebit/Object.h
     setSolidness(SPECTRAL);
+
+    // We set the velocity of the bubble to float up the screen
     setVelocity(Vector(0, -1));
-    setPosition(position);
+
+    // Altitude is a property of the object that determines the order in which
+    // objects are drawn on the screen. The higher the altitude, the later the
+    // object is drawn. This is useful when you want to draw objects on top of
+    // others.
     setAltitude(2);
+
+    // We set the position of the bubble to the position we received in the
+    // constructor
+    setPosition(position);
+
+    // We subscribe to the OUT_EVENT here, to mark the bubble for deletion when
+    // it goes out of the screen. We will handle this in the eventHandler()
     subscribe(OUT_EVENT);
   }
 
+  // This is where we handle the events we subscribed to.
+  // In this case, we are only interested in the OUT_EVENT, which we use to mark
+  // the bubble for deletion when it goes out of the screen.
   int eventHandler(const Event *p_e) override {
     if (p_e->getType() == OUT_EVENT) {
       WM.markForDelete(this);
@@ -31,11 +54,25 @@ public:
 class Fish : public Object {
 public:
   Fish() {
+    // This is your first character!
+    // It is a fish that swims in the middle of the screen and honestly does not
+    // do much. We give it a type and assign it a sprite that we have loaded in
+    // the main.cpp. You can pick the sprites by their label ("fish" in this
+    // case)
     setType("Fish");
     setSprite("fish");
+
+    // This is how you can make your game objects interactive.
+    // By subscribing to events, you can make your objects react to them.
+    // Here we are subscribing to the STEP_EVENT, which is triggered every
+    // frame. We are using it to spawn bubbles every now and then (see
+    // eventHandler())
     subscribe(STEP_EVENT);
   }
 
+  // This is where we handle the events we subscribed to.
+  // In this case, we are only interested in the STEP_EVENT, which we use to
+  // spawn bubbles from time to time.
   int eventHandler(const Event *p_e) override {
     if (p_e->getType() == STEP_EVENT) {
       if (rand() % 100 == 0) {
--- src/scenes/GameScene.cpp
+++ src/scenes/GameScene.cpp
@@ -11,12 +11,15 @@ class WelcomeText : public Object {
 public:
   WelcomeText() { this->setType("WelcomeText"); }
 
+  // The draw method is called every frame to draw the object on the screen.
   int draw() override {
     int result = 0;
 
-    // This comes from the CMakeLists.txt file
+    // This comes from the CMakeLists.txt file, don't mind it.
     std::string version = LATEBIT_VERSION;
 
+    // Here we are using the Display Manager (DM) to draw text on the screen at
+    // a given position.
     result += DM.drawString(this->getPosition() + Vector(0, 30), version,
                             TEXT_ALIGN_CENTER, WHITE);
     return result;
@@ -26,6 +29,8 @@ public:
 class Logo : public Object {
 public:
   Logo() {
+    // This is the simplest object you can create: you give it a type and a
+    // sprite, and, by using `setPosition` you can place it on the screen.
     this->setType("Logo");
     this->setSprite("logo");
   }
@@ -39,15 +44,24 @@ private:
 
 public:
   GameScene() {
+    // The scene is a container object for all the objects that will be drawn on
+    // the screen.
+    // Setting the type will allow you to retrieve the scene later on and to
+    // identify it in the logs.
     setType("GameScene");
 
+    // The Display Manager (DM) is responsible for drawing everything on the
+    // screen. Here you can set the background color of the scene for example.
     DM.setBackground(Color::BLUE);
-    auto music = RM.getMusic("music");
 
+    // Here we are retrieving the music we loaded in the main.cpp from the
+    // ResourceManager (RM) and playing it.
+    auto music = RM.getMusic("music");
     if (music != nullptr) {
       music->play();
     }
 
+    // In the following block we are placing objects in the scene.
     auto center =
         Vector(DM.getHorizontalCells() / 2.0, DM.getVerticalCells() / 2.0);
 
